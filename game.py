2e1f62bff24a97a539bbf7ec3750fb14b073f8ce
lucad <luca.drouillet@gmail.com>
2026-04-10 13:29:03 +0200

    changing taking steps for the game

--- game.py
+++ game.py
@@ -23,9 +23,9 @@ class Game:
             if (self.leader.time <= self.follower.time and self.leader.time >= self.follower.previous_time) or (self.follower.time <= self.leader.time and self.follower.time >= self.leader.previous_time):
                 return True
         return False
-
-
-    def play_game(self, step_cap = 100, debug = False) -> int | None:
+    
+    
+    def play_game(self, step_cap = 100, debug = False) -> tuple[int, int] | None:
         '''
         Plays the game step by step, checking the capture condition at every step. 
         Returns step at which the leader captured the follower
@@ -33,21 +33,25 @@ class Game:
         if debug == True:
             print(f"The leader starts at state {self.leader.state} and the follower starts at state {self.follower.state}")
         capture = False
-        step = 0
-        while not capture and step < step_cap: #step_cap wasn't used before
-            step += 1
-            self.leader.take_step()
-            if debug == True:
-                print(f"the leader end up at state {self.leader.state} at time {self.leader.time}")
-            self.follower.take_step()
-            if debug == True:
-                print(f"the follower end up at state {self.follower.state} at time {self.follower.time}")
+        leader_step = 0
+        follower_step = 0
+        while not capture and leader_step < step_cap and follower_step < step_cap:
+            if self.follower.time < self.leader.time:
+                follower_step += 1
+                self.follower.take_step()
+                if debug == True:
+                    print(f"the follower end up at state {self.follower.state} at time {self.follower.time} and step {follower_step}")
+            else:
+                leader_step += 1
+                self.leader.take_step()
+                if debug == True:
+                    print(f"the leader end up at state {self.leader.state} at time {self.leader.time} and step {leader_step}")
             capture = self.check_capture()
             if capture:
                 if debug == True:
-                    print(f"The game ends at step {step}")
+                    print(f"The game ends at step {leader_step} for the leader and step {follower_step} for the follower")
             if debug == True:
                 print("\n")
         if capture:
-            return step
-        return None
+            return leader_step, follower_step
+        return None 
